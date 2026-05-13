36b1b29a01fe1690c554173644a63f2eed8e771b
ltcontadores <oscar@ltcontadores.com>
2026-05-13 17:24:29 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = document.querySelectorAll(".fade-in");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  });
+
+  elements.forEach((element) => {
+    observer.observe(element);
+  });
+});
